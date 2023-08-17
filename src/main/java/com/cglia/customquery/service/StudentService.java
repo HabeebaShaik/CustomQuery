9b06e37c8f15c035f8b1c3bb337afd00452a51ed
Habeeba Shaik <habeeba.shaik@cglia.com>
2023-08-17 18:36:24 +0530

    Custom query and auditing

---
+++ src/main/java/com/cglia/customquery/service/StudentService.java
@@ -0,0 +1,99 @@
+package com.cglia.customquery.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.cglia.customquery.model.Student;
+import com.cglia.customquery.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	@Autowired
+	private StudentRepository studentRepository;
+
+	public List<Student> listAll() {
+		return studentRepository.findAll();
+	}
+
+	public void save(Student student) {
+		studentRepository.save(student);
+	}
+
+	public void delete(Long id) {
+		studentRepository.deleteById(id);
+
+	}
+
+	public String update(Long id, Student student) {
+		Optional<Student> existingStudentOptional = studentRepository.findById(id);
+
+		if (existingStudentOptional.isPresent()) {
+			Student existingStudent = existingStudentOptional.get();
+			existingStudent.setFirstName(student.getFirstName());
+			existingStudent.setLastName(student.getLastName());
+			existingStudent.setAge(student.getAge());
+			existingStudent.setEmail(student.getEmail());
+			existingStudent.setDob(student.getDob());
+			existingStudent.setPhone(student.getPhone());
+			existingStudent.setCreatedBy(student.getCreatedBy());
+
+			studentRepository.save(existingStudent);
+			return "Updated successfully";
+		} else {
+			return "Student with ID " + id + " not found";
+		}
+	}
+
+	 public Student getStudentById(Long id) {
+	        return studentRepository.findById(id).orElse(null);
+	    }
+
+	public List<Student> getStudentsWithAgeLessThan(Integer age) {
+		return studentRepository.findStudentsWithAgeLessThan(age);
+	}
+
+	public List<Student> getStudentsByLastnameAndFirstname(String lastName, String firstName) {
+		return studentRepository.findStudentsByLastnameAndFirstname(lastName, firstName);
+	}
+
+	public List<Student> getStudentsByFirstnameLike(String firstName) {
+		return studentRepository.findStudentsByFirstnameLike(firstName);
+	}
+
+	public List<Student> findStudentsByLastNameOrFirstName(String lastName, String firstName) {
+		return studentRepository.findStudentsByLastnameOrFirstname(lastName, firstName);
+	}
+
+	public List<Student> getStudentsWithAgeGreaterThan(Integer age) {
+		return studentRepository.findStudentsWithAgeGreaterThan(age);
+	}
+
+	public List<Student> findStudentsByDob(Date dob) {
+		return studentRepository.findStudentsByDob(dob);
+	}
+
+	public List<Student> findStudentsByDobBetween(Date dobFrom, Date dobTo) {
+		return studentRepository.findStudentsByDobBetween(dobFrom, dobTo);
+	}
+
+	public List<Student> findByFirstnameEndingWith(String firstName) {
+		return studentRepository.findByFirstnameEndingWith(firstName);
+	}
+
+	public Long countStudentsByAgeGreaterThan(Integer age) {
+		return studentRepository.countStudentsByAgeGreaterThan(age);
+	}
+	
+	 public Page<Student> findByDobGreaterThan(Date dob, int page, int size) {
+	        Pageable pageable = PageRequest.of(page, size);
+	        return studentRepository.findByDobGreaterThan(dob, pageable);
+	    }
+
+}
